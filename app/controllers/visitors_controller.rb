3e370680fa5fc29449e1116e9d3a5d32e6ad8622
Cloud User <ec2-user@ip-172-31-1-95.ap-southeast-2.compute.internal>
2015-10-27 21:49:46 +1100

    Update stuff

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -11,14 +11,14 @@ class VisitorsController < ApplicationController
   def get_sandra
     day_count = Reading.where(['created_at >= ?', DateTime.now.beginning_of_day]).count
 
-    hourly_count = Reading.where(['created_at >= ?', 30.minutes.ago]).count
+    hourly_count = Reading.where(['created_at >= ?', 30.seconds.ago]).count
 
     if day_count == 0
       return SANDRA[(0..2).to_a.sample]
     elsif hourly_count > 19
       return SANDRA[(21..23).to_a.sample]
     else
-      return SANDRA[hourly_count+3]
+      return SANDRA[hourly_count+2]
     end
   end
 end
